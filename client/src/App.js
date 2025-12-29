fc3b470d5054bb79f9ce862784e0289b6bb797e1
Kuttivel-Dharmaraj <kuttiveldharmaraj@gmail.com>
2025-12-29 02:13:00 +0000

    Display static task list in UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const tasks = [
+                 "Walk 50 km",
+                 "Sleep 8 hours",
+                 "Eat"
+                ];
+
+  return(
+    <div>
+          <h1>Task Manager</h1>
+          
+          <ul>
+            {tasks.map((task, index) => (
+              <li key = {index}>{task}</li>
+            ))}
+          </ul>
     </div>
   );
 }
